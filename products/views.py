0a23ebf913a58acd08d9a1817dd15fed57897a63
JackLamb99 <jacklamb15@yahoo.co.uk>
2026-03-08 18:40:40 +0000

    ADD shop view with category filtering and sorting

--- products/views.py
+++ products/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import Product, Category
 
-# Create your views here.
+
+def shop(request):
+    products = Product.objects.all()
+    categories = Category.objects.all()
+
+    category_slug = request.GET.get("category")
+    sort = request.GET.get("sort")
+
+    # Category filtering
+    if category_slug and category_slug != "all":
+        products = products.filter(category__slug=category_slug)
+
+    # Sorting
+    if sort == "price_asc":
+        products = products.order_by("price")
+    elif sort == "price_desc":
+        products = products.order_by("-price")
+    elif sort == "name":
+        products = products.order_by("name")
+    elif sort == "newest":
+        products = products.order_by("-created_at")
+    elif sort == "oldest":
+        products = products.order_by("created_at")
+
+    context = {
+        "products": products,
+        "categories": categories,
+    }
+
+    return render(request, "products/shop.html", context)
