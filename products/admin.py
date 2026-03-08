ef5cd64ca909d71c131b7da8d35cc2616f37f483
JackLamb99 <jacklamb15@yahoo.co.uk>
2026-03-08 18:23:04 +0000

    ADD products admin with inline product images

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage
 
-# Register your models here.
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 5
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "category",
+        "price",
+        "sale_price",
+        "stock_quantity",
+        "created_at",
+    )
+    list_filter = ("category", "created_at")
+    search_fields = ("name", "description")
+    prepopulated_fields = {"slug": ("name",)}
+    inlines = [ProductImageInline]
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ("product", "is_primary", "sort_order")
+    list_filter = ("is_primary",)
