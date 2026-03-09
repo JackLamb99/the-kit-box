bd25bf27bbc46a61dae3fdb28eb585bfb45b89f5
JackLamb99 <jacklamb15@yahoo.co.uk>
2026-03-09 16:39:58 +0000

    UPDATE accounts urls to handle my account page paths

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+
 app_name = "accounts"
 
 urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path("my-account/", views.account_view, name="account"),
+    path("orders/<str:order_number>/", views.account_order_detail, name="order_detail"),
 ]
