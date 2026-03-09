1d48e3e19e561d93606d1f9b9ed18d6bd40fff19
JackLamb99 <jacklamb15@yahoo.co.uk>
2026-03-09 01:18:30 +0000

    ADD home and accounts urls

--- the_kit_box/urls.py
+++ the_kit_box/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import home
+
 
 urlpatterns = [
+    path("", home, name="home"),
     path('admin/', admin.site.urls),
+    path("accounts/", include("accounts.urls")),
     path("shop/", include("products.urls")),
 ]
 
