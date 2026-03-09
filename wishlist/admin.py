ebc591d2992e4e3578bedbfee4bbc7f692ccde59
JackLamb99 <jacklamb15@yahoo.co.uk>
2026-03-09 07:57:50 +0000

    ADD logic to allow users to toggle wishlist status when logged-in

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import WishlistItem
 
-# Register your models here.
+
+@admin.register(WishlistItem)
+class WishlistItemAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__email', 'product__name')
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from products.models import Product
+from .models import WishlistItem
 
-# Create your views here.
+
+@login_required
+def toggle_wishlist(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    wishlist_item = WishlistItem.objects.filter(
+        user=request.user,
+        product=product
+    ).first()
+
+    if wishlist_item:
+        wishlist_item.delete()
+    else:
+        WishlistItem.objects.create(
+            user=request.user,
+            product=product
+        )
+
+    return redirect(request.META.get('HTTP_REFERER', 'shop'))
+
+
+@login_required
+def wishlist_view(request):
+    wishlist_items = WishlistItem.objects.filter(user=request.user).select_related('product')
+
+    return render(request, 'wishlist/wishlist.html', {
+        'wishlist_items': wishlist_items,
+    })
