aeb084ea63e5e0c4fdf0f0a617a9602d6e98b9e1
JackLamb99 <jacklamb15@yahoo.co.uk>
2026-03-09 01:21:55 +0000

    UPDATE product views to refresh when new filtering or sorting option is selected

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 
@@ -6,20 +6,20 @@ def shop(request):
     products = Product.objects.all()
     categories = Category.objects.all()
 
-    category_slug = request.GET.get("category")
-    sort = request.GET.get("sort")
+    category_slug = request.GET.get("category", "all")
+    sort = request.GET.get("sort", "name")
 
     # Category filtering
-    if category_slug and category_slug != "all":
+    if category_slug != "all":
         products = products.filter(category__slug=category_slug)
 
     # Sorting
-    if sort == "price_asc":
+    if sort == "name":
+        products = products.order_by("name")
+    elif sort == "price_asc":
         products = products.order_by("price")
     elif sort == "price_desc":
         products = products.order_by("-price")
-    elif sort == "name":
-        products = products.order_by("name")
     elif sort == "newest":
         products = products.order_by("-created_at")
     elif sort == "oldest":
@@ -28,6 +28,18 @@ def shop(request):
     context = {
         "products": products,
         "categories": categories,
+        "current_category": category_slug,
+        "current_sort": sort,
     }
 
     return render(request, "products/shop.html", context)
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+
+    context = {
+        "product": product,
+    }
+
+    return render(request, "products/product_detail.html", context)
