2c48a0e3424deb95f6fd59323f69e982342403eb
JackLamb99 <jacklamb15@yahoo.co.uk>
2026-03-09 16:39:27 +0000

    UPDATE views to handle inactive product functionality

--- wishlist/views.py
+++ wishlist/views.py
@@ -6,7 +6,7 @@ from .models import WishlistItem
 
 @login_required
 def toggle_wishlist(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
+    product = get_object_or_404(Product, id=product_id, is_active=True)
 
     wishlist_item = WishlistItem.objects.filter(
         user=request.user,
@@ -21,13 +21,16 @@ def toggle_wishlist(request, product_id):
             product=product
         )
 
-    return redirect(request.META.get('HTTP_REFERER', 'shop'))
+    return redirect(request.META.get("HTTP_REFERER", "shop"))
 
 
 @login_required
 def wishlist_view(request):
-    wishlist_items = WishlistItem.objects.filter(user=request.user).select_related('product')
+    wishlist_items = WishlistItem.objects.filter(
+        user=request.user,
+        product__is_active=True
+    ).select_related("product")
 
-    return render(request, 'wishlist/wishlist.html', {
-        'wishlist_items': wishlist_items,
+    return render(request, "wishlist/wishlist.html", {
+        "wishlist_items": wishlist_items,
     })
